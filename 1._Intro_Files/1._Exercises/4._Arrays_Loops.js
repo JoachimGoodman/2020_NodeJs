a17a32c814dfb9df87834587c8cd2326668c549c
JoachimGoodman <joac1087@stud.kea.dk>
2020-02-09 20:07:28 +0100

    test

--- 1._Intro_Files/1._Exercises/4._Arrays_Loops.js
+++ 1._Intro_Files/1._Exercises/4._Arrays_Loops.js
@@ -2,6 +2,7 @@
 // Arrays, for loops
 // --------------------------------------
 // Exercise 1 - Array Positioning
+console.log("\n-----------------Exercise 1 -----------------")
 
 var letters = ["a","b","c"];
 // show b in the console 
@@ -10,6 +11,7 @@ console.log(letters[1])
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
+console.log("\n-----------------Exercise 2 -----------------")
 
 var friends = [];
 
@@ -23,11 +25,13 @@ console.log(friends)
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
+console.log("\n-----------------Exercise 3 -----------------")
 
 var significantMathNumbers = [0, 2.718, 3.14159, 1729]
 
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
+
 
 // for(i = 0; i < significantMathNumbers.length; i++) {
 //     if(significantMathNumbers[i] === 1729) {
@@ -39,6 +43,7 @@ significantMathNumbers.indexOf(1729)
 console.log(significantMathNumbers)
 // --------------------------------------
 // Exercise 4 - Inserting elements
+console.log("\n-----------------Exercise 4 -----------------")
 
 var diet = ["tomato", "cucumber", "rocolla", "kale"]
 
@@ -50,6 +55,7 @@ console.log(diet)
 
 // --------------------------------------
 // Exercise 5 - Remove element
+console.log("\n-----------------Exercise 5 -----------------")
 
 // You don't like kale at all. Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
@@ -69,19 +75,22 @@ console.log(diet)
 
 // --------------------------------------
 // Exercise 6 - Copy array
+console.log("\n-----------------Exercise 6 -----------------")
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-var dinnerTray = diet
+// (insert position, start position, end position)
+var dinnerTray = diet.copyWithin() // ?
 
-var dinnerTray = diet.splice(*, 0) // ?
+var dinnerTray = diet // ?
 
-console.log("dinnerTray", dinnerTray)
+console.log("dinnerTray: ", dinnerTray)
 
 // --------------------------------------
 
 // --------------------------------------
 // Exercise 7 - For loop
+console.log("\n-----------------Exercise 7 -----------------")
 
 var letters = ["a","b","c", "d", "e", "f", "g", "h"];
 
@@ -89,10 +98,11 @@ var letters = ["a","b","c", "d", "e", "f", "g", "h"];
 
 for(i = 0; i < letters.length; i+=2) {
     console.log(letters[i])
-}
+} 
 
 // --------------------------------------
 // Exercise 8 - For loop and if statement
+console.log("\n-----------------Exercise 8 -----------------")
 
 var numbers  = [5, 3, 2, 7, 11, 12, 0, -20, 6];
 
@@ -101,10 +111,13 @@ var discardedNumbers = [];
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
 
-for(i = 0; i < numbers; i++) {
-    if(numbers[i] < 6 && numbers[i] > 0) {
+for(i = 0; i < numbers.length; i++) {
+    if(numbers[i] >= 6 || numbers[i] <= 0) {
         console.log(numbers[i])
+    } else {
+        discardedNumbers.push(numbers[i])
     }
 }
+console.log("DiscardedNumbers: ", discardedNumbers)
 
 // --------------------------------------
