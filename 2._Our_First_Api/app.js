482c0cdde8f3115ea665205ae0005e5cf2c07a45
JoachimGoodman <joac1087@stud.kea.dk>
2020-02-14 14:00:40 +0100

    changed app.js

--- 2._Our_First_Api/app.js
+++ 2._Our_First_Api/app.js
@@ -20,6 +20,21 @@ app.get("/aboutMe", (req, res) => {
     res.send(response)
 })
 
+const daysArray = ["monday", "tuesday", "wednesday", "Thursday", "Freday", "saturday", "sunday"]
+
+app.get("/time", (req, res) => {
+    let current = new Date()
+    const response = {
+        day: daysArray[current.getDay() - 1],
+        hour: current.getHours(),
+        minute: current.getMinutes(),
+        seconds: current.getSeconds(),
+        milliseconds: current.getMilliseconds(),
+        weather: "sunny"
+    }
+    res.send(response)
+})
+
 // this listen on PORT 3000
 app.listen(3000, (error) => {
     if (error) {
